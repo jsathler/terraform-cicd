8dad6513e2c999e237d906298dc6c9dc78706d8d
Juliano Sathler <jsathler@live.com>
2024-02-16 10:50:22 -0300

    TF CI Workflow

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,3 +6,7 @@ resource "azurerm_resource_group" "default" {
   name     = "${var.project_name}-${var.environment}-rg"
   location = var.location
 }
+
+output "rg_id" {
+  value = azurerm_resource_group.default.id
+}
